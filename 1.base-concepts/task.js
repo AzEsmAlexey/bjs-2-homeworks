dfa7249048d15fb6b9e893524d12b11b69f44fd7
Neto.Changes <infero-sky@yandex.ru>
2023-12-30 01:58:22 +0300

    The homework is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
+  let d = b ** 2 - 4 * a * c;
   let arr = [];
-  
-  return arr;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+      let x = -b / (2 * a);
+      arr.push(x);
+      return arr;
+  } else {
+      let x1 = (-b + Math.sqrt(d)) / (2 * a);
+      let x2 = (-b - Math.sqrt(d)) / (2 * a);
+      arr.push(x1, x2);
+      return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = percent / 100 / 12;
+  let S = amount - contribution;
+  let payment = S * (p + (p / (((1 + p) ** countMonths) - 1)));
+  let totalAmount = payment * countMonths;
+  return +totalAmount.toFixed(2);
+
 }
