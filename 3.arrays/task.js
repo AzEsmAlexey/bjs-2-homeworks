98889803340dfeb18d6df0b9a21f9b16371cd917
Neto.Changes <infero-sky@yandex.ru>
2024-01-22 00:55:19 +0300

    Homework correction

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -5,6 +5,9 @@ function compareArrays(arr1, arr2) {
 
 function getUsersNamesInAgeRange(users, gender) {
   const filteredUsers = users.filter(user => user.gender === gender);
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
   const ages = filteredUsers.map(user => user.age);
   const sum = ages.reduce((acc, age) => acc + age, 0);
   const averageAge = sum / ages.length;
