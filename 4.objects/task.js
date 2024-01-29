fc381c41fb53fe000cf33f5ed2405c852f679e9d
Neto.Changes <infero-sky@yandex.ru>
2024-01-30 01:30:30 +0300

    Correction Objects 2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -19,8 +19,7 @@ Student.prototype.getAverage = function() {
   if (this.marks.length === 0) {
     return 0;
   }
-  this.average = this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
-  return this.average;
+  this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function(reason) {
@@ -29,3 +28,14 @@ Student.prototype.exclude = function(reason) {
   this.excluded = reason;
 }
 
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage());
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
