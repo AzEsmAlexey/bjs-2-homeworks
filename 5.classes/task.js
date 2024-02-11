d8f83b67c6e005804ab7fc259e6d738f44cb75ae
Neto.Changes <infero-sky@yandex.ru>
2024-02-11 23:33:20 +0300

    Add third exercise

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -129,3 +129,37 @@ library.addBook(
 );
 console.log(library.books);
 console.log("Количество книг после выдачи: " + library.books.length);
+
+class student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
+
+  addmark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      return;
+    }
+    if (!this.marks.hasOwnProperty(subject)) {
+      this.marks[subject] = [];
+    }
+    this.marks[subject].push(mark);
+  }
+  getAveragBySubject(subject) {
+    if (!this.marks.hasOwnProperty(subject)) {
+      return 0;
+    }
+    return this.marks[subject].reduce(
+        (acc, mark) => acc + mark / this.marks[subject].length, 
+      0); //Находим среднее значение оценок по предмету, каждый элемент делим на длину массива и суммируем
+  }
+  getAverage() {
+    const subjects = Object.keys(this.marks); //Ключи массива с предметами
+    if (subjects.length === 0) {
+      return 0;
+    }
+    return subjects.reduce(
+      (acc, subject) => acc + this.getAveragBySubject(subject) / subjects.length,
+      0); //Находим среднее значение оценок по всем предметам, каждый элемент делим на длину массива и суммируем
+  }
+}
