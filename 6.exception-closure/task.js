2788d86bf7496451322cf6d8eaafd7d41cebcab4
Neto.Changes <infero-sky@yandex.ru>
2024-02-14 02:26:31 +0300

    Add HW Exception Closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,11 +1,54 @@
 ﻿'use strict';
 
 function parseCount(count) {
-  const parsedCount = parseFloat(count);
-  if(isNaN(parseCount)) {
+  const result = parseFloat(count);
+  if (isNaN(result)) {
     throw new Error("Невалидное значение");
-  } 
-  return parsedCount;
+  }
+  return result;
 }
 
-console.log(parseCount(123));
+function validateCount(count) {
+  try {
+    return parseCount(count);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        "Ошибка! Треугольник не существует";
+      },
+      get Area() {
+        "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
