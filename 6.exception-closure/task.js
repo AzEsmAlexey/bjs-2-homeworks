9471c84df20aee04f995de65c50eb2078bae60a1
Neto.Changes <infero-sky@yandex.ru>
2024-02-14 00:56:09 +0300

    TMP

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,11 @@
-﻿
+﻿'use strict';
+
+function parseCount(count) {
+  const parsedCount = parseFloat(count);
+  if(isNaN(parseCount)) {
+    throw new Error("Невалидное значение");
+  } 
+  return parsedCount;
+}
+
+console.log(parseCount(123));
