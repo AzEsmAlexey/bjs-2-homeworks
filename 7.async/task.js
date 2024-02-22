b91b3e09a67267ae3bb876f2a10f343c22851219
Neto.Changes <infero-sky@yandex.ru>
2024-02-23 00:44:26 +0300

    add HW async

---
+++ 7.async/task.js
@@ -0,0 +1,63 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error("Отсутствуют обязательные аргументы");
+    }
+
+    if (this.alarmCollection.find(item => item.time === time)) {
+      console.warn("Уже присутствует звонок на это же время");
+    }
+
+    this.alarmCollection.push({
+      callback,
+      time,
+      canCall: true
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    return hours + ":" + minutes;
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+      this.alarmCollection.forEach((item) => {
+        if (item.time === currentTime && item.canCall) {
+          item.callback();
+          item.canCall = false;
+        }
+      })
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach((item) => item.canCall = true);
+  }
+
+  cleanAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
